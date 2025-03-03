dba48f8402400ebbc54c9e7ae295f7a2f699e37e
FApew <112774976+FApew@users.noreply.github.com>
2025-03-03 15:38:02 +0100

    RemovedNodeJS AddedStarTracker

--- src/data.js
+++ src/data.js
@@ -1,13 +1,17 @@
 class option {
-    constructor(name, type, defaultV) {
+    constructor(name, type, defaultV, min, max, step) {
         this.name = name
         this.type = type
         this.defaultV = defaultV
+        this.min = min
+        this.max = max
+        this.step = step
     }
 }
 
 export let options = [
-    new option("Mode", "cbx", 0),
+    new option("RGB Mode", "cbx", 1),
     new option("K value", "num", null),
-    new option("Sensibility", "rng", 30),
+    new option("Sensibility", "rng", 30, 0, 100, 5),
+    new option("Size", "rng", 15, 0, 50, 1),
 ]
--- src/script.js
+++ src/script.js
@@ -21,6 +21,7 @@ uplArr.forEach((upl) => {
         let idx = event.target.id.replace("inUpl", "")
 
         if (file && file.type.startsWith("image/")) {
+            
             const reader = new FileReader()
             reader.readAsDataURL(file);
 
@@ -63,10 +64,9 @@ window.addEventListener("resize", () => {
         cnv.height = cnvBoxes[0].clientHeight
 
         if (imgs[cnv.id]) {
-            let ctx = cnv.getContext("2d")
             let img = imgs[cnv.id]
-            let args = getImgArgs(img)
-            ctx.drawImage(img, args[0], args[1], args[2], args[3])
+
+            drawImage(cnv, img)
         }
     })
 })
@@ -77,21 +77,18 @@ cancButtons.forEach((canc) => {
 
         imgs[idx] = null
         boxArr[idx].style.visibility = "hidden"
+        document.getElementById(`inUpl${idx}`).files = new DataTransfer().files
     })
 })
 
 function insertImg(idx) {
     let cnv = cnvArr[idx]
     let box = boxArr[idx]
-    let ctx = cnv.getContext("2d")
     let img = imgs[idx]
 
     box.style.visibility = "visible"
     
-    ctx.fillStyle = "#0c0d25"
-    ctx.fillRect(0,0,cnv.width,cnv.height)
-    let args = getImgArgs(img)
-    ctx.drawImage(img, args[0], args[1], args[2], args[3])
+    drawImage(cnv, img)
 }
 
 function getImgArgs(img) {
@@ -104,15 +101,25 @@ function getImgArgs(img) {
 
     if (boxAR > imgAR) {
         h = box.clientHeight
-        w = h*imgAR
-        ox = box.clientWidth/2-w/2
+        w = Math.floor(h*imgAR)
+        ox = Math.floor(box.clientWidth/2-w/2)
     } else {
         w = box.clientWidth
-        h = w/imgAR
-        oy = box.clientHeight/2-h/2
+        h = Math.floor(w/imgAR)
+        oy = Math.floor(box.clientHeight/2-h/2)
     }
 
     return [ox, oy, w, h]
+}
+
+function drawImage(cnv, img) {
+    let ctx = cnv.getContext("2d")
+    ctx.fillStyle = "#0c0d25"
+    ctx.fillRect(0,0,cnv.width,cnv.height)
+
+    let args = getImgArgs(img)
+    
+    ctx.drawImage(img, args[0], args[1], args[2], args[3])
 }
 
 /* ------------------------------- //!SECTION ------------------------------- */
@@ -151,11 +158,17 @@ options.forEach((opt) => {
 
     switch (opt.type) {
         case "cbx": {
+            let oCbxSwitch = document.createElement("div")
+
             oIn.type = "checkbox"
             oInBox.classList.add("oCbxBox")
             oIn.classList.add("oCbx")
+            oCbxSwitch.classList.add("oCbxSwitch")
+
+            if (opt.defaultV) oIn.checked = true
 
             oInBox.appendChild(oIn)
+            oInBox.appendChild(oCbxSwitch)
 
             break
         }
@@ -170,15 +183,33 @@ options.forEach((opt) => {
             break
         }
         case "rng": {
+            const oRngDisplay = document.createElement("div")
+
             oIn.type = "range"
             oInBox.classList.add("oRngBox")
             oIn.classList.add("oRng")
-            
+            oRngDisplay.classList.add("oRngDisplay")
+
+            oIn.min = opt.min
+            oIn.max = opt.max
+            oIn.step = opt.step
+
+            oIn.value = opt.defaultV
+
+            oIn.addEventListener("input", (e) => {
+                refreshSlider(e.target)
+            })
+
             oInBox.appendChild(oIn)
+            oInBox.appendChild(oRngDisplay)
+
+            refreshSlider(oIn)
 
             break
         }
     }
+
+    oIn.id = Array.from(opt.name.toLowerCase()).map((a, idx, arr) => {if (a == " ") {a = ""; arr[idx + 1] = arr[idx + 1].toUpperCase()} return a}).join("").slice(0, 4)
 
     oInBox.classList.add("oInBox")
     oBox.classList.add("oBox")
@@ -193,5 +224,129 @@ options.forEach((opt) => {
 })
 
 optBox.style.gridTemplateRows = temp
+
+function refreshSlider(elm) {
+    elm.style.backgroundPosition = `-${elm.value/elm.max*70 + 15}%`
+    let display = elm.parentElement.querySelector(".oRngDisplay")
+    display.innerHTML = `${Math.round(elm.value)}`
+    display.style.left = `calc(${(elm.value/elm.max*70 + 15)}% - 13px)`
+}
+
+/* ------------------------------- //!SECTION ------------------------------- */
+
+/* ---------------------- //SECTION - Image Processing ---------------------- */
+
+const submit = document.getElementById("submit")
+submit.addEventListener("click", () => {
+    if(imgs[0]/* && imgs[1] && imgs[2]*/) {
+        imgs.forEach((img, idx) => {
+
+        })
+        let idx = 0, img = imgs[0]
+        getStarArr(img, idx)
+    }
+})
+
+function getStarArr(img, idx) {
+    let cnv = cnvArr[idx]
+    let ctx = cnv.getContext("2d", { willReadFrequently: true })
+    drawImage(cnv, img)
+
+    let w = cnv.width, h = cnv.height
+
+    let starsArr = []
+    
+    /*let imgCnv = document.createElement("canvas")
+    imgCnv.width = w
+    imgCnv.height = h
+    let imgCtx = imgCnv.getContext("2d")
+    imgCtx.drawImage(img, 0, 0, w, h)
+
+    let pixels = imgCtx.getImageData(0, 0, w, h).data*/
+
+    let args = getImgArgs(img)
+    let ox = args[0], oy = args[1]
+    let pixels = ctx.getImageData(args[0], args[1], args[2], args[3]).data
+
+    let sens = parseFloat(document.getElementById("sens").value)/100, size = parseFloat(document.getElementById("size").value), sens2 = .98
+
+    for (let y = 0; y < h; y++) {
+        for (let x = 0; x < w; x++) {
+            if (rgbToLum(pxVal(x, y)) > 255*(1-sens) && search(x, y)) {
+                let lum = rgbToLum(pxVal(x, y))
+                let leftX = x, rightX = x
+                do {
+                    leftX--
+                } while (rgbToLum(pxVal(leftX, y)) > 255*(1-sens));
+
+                do {
+                    rightX++
+                } while (rgbToLum(pxVal(rightX, y)) > 255*(1-sens));
+
+                let midX = Math.floor((leftX+rightX)/2)
+                
+                let topY = y, botY = y
+
+                do {
+                    topY--
+                } while (rgbToLum(pxVal(midX, topY)) > 255*(1-sens));
+
+                do {
+                    botY++
+                } while (rgbToLum(pxVal(midX, botY)) > 255*(1-sens));
+
+                let midY = Math.floor((botY + topY)/2)
+
+                topY = midY, botY = midY
+                lum = rgbToLum(pxVal(midX, midY)) 
+
+                do {
+                    topY--
+                } while (rgbToLum(pxVal(midX, topY)) > lum*sens2);
+
+                do {
+                    botY++
+                } while (rgbToLum(pxVal(midX, botY)) > lum*sens2);
+
+                starsArr.push([midX, midY, Math.abs(topY-botY)])
+            }
+        }
+    }
+
+    starsArr.forEach((star) => {
+        let x = star[0], y = star [1]
+
+        ctx.strokeStyle = "#aa00007f"
+        ctx.beginPath()
+        ctx.arc(x+ox, y+oy, size, 0, 2*Math.PI)
+        ctx.stroke()
+
+        ctx.strokeStyle = "#a17fffaf"
+        ctx.beginPath()
+        ctx.arc(x+ox, y+oy, star[2], 0, 2*Math.PI)
+        ctx.stroke()
+
+        console.log(star)
+    })
+
+    return starsArr
+
+    function pxVal(x, y) {
+        return [pixels[(y*w+x)*4], pixels[(y*w+x)*4+1], pixels[(y*w+x)*4+2]]
+    }
+
+    function rgbToLum(rgb) {
+        return 0.2126*rgb[0] + 0.7152*rgb[1] + 0.0722*rgb[2]
+    }
+
+    function search(x, y) {
+        for (let star of starsArr) {
+            if (Math.hypot(x-star[0], y-star[1]) < size+star[2]){
+                return false
+            }
+        }
+        return true
+    }
+}
 
 /* ------------------------------- //!SECTION ------------------------------- */
